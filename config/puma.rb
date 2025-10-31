25276d966fc486cb1ccae81d8708eddbf4992c94
hgkimdev <hgkim2738@gmail.com>
2025-10-31 16:38:19 +0900

    puma.rb-오타

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 port ENV.fetch("PORT") { 3000 }
-environments ENV.fetch("RAILS_ENV") { ENV["RACK_ENV"] || "development" }
+environment ENV.fetch("RAILS_ENV") { ENV["RACK_ENV"] || "development" }
 pidfile ENV.fetch("FIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
