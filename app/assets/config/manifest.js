a18652953099ebdcfaf6ee95839f19d319e0eeb0
hgkimdev <hgkim2738@gmail.com>
2025-11-03 20:46:24 +0900

    11-03

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,13 @@
-// = link_tree ../images
-// = link_directory ../javascripts .js
-// = link application.css
+// app/assets/config/manifest.js
 
-// = link rails.svg
-// = link kitten.jpg
+//= link_tree ../images
+//= link application.css
+
+//= link rails.svg
+//= link kitten.jpg
+
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
+
+// // = link_directory ../javascripts .js  
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      flash.now[:danger] = "Invalid email or password." # Not quite right!
       render "new", status: :unprocessable_entity
     end
   end
