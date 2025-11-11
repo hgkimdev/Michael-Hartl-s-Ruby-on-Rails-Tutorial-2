621a17a345aa76407b31b15748ecfacf6d780a7d
hgkimdev <hgkim2738@gmail.com>
2025-11-11 13:17:05 +0900

    Remember-tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user &.authenticate(params[:session][:password])
       reset_session
-      params[:session][:remember_me] == 1 ? remember(user) : forget(user)
-      remember user
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
       # Log the user in and redirect to the user's show page.
     else
       flash.now[:danger] = "Invalid email or password." # Not quite right!
