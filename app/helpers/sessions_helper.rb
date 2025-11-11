d87c0658403734d3398d64b7bbb092e5191f1ae1
hgkimdev <hgkim2738@gmail.com>
2025-11-11 17:51:41 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    # Guard against session replay attacks.
+    # See https://bit.ly/33UvK0w for more.
+    session[:session_token] = user.session_token
   end
   # Remembers a user in a persistent session.
   def remember(user)
@@ -14,9 +17,13 @@ module SessionsHelper
   #     @current_user ||= User.find_by(id: session[:user_id])
   #   end
   # end
+
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
